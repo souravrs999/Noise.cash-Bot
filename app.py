142ab37bcd07d47cee90fe59f69a7a38442cb372
sourav <souravraveendran6@gmail.com>
2021-02-24 13:48:39 +0530

    memory error

--- app.py
+++ app.py
@@ -327,7 +327,6 @@ if __name__ == "__main__":
                 bot.randomTip()
                 bot.changeWallet()
                 bot.Logout()
-                time.sleep(30)
             except Exception as e:
                 print(f"--- Error {e}")
                 bot.Close()
